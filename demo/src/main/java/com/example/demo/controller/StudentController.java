19551bf4c24c3afe3164e706e9e0226590952687
jeyhan22 <74844330+jeyhan22@users.noreply.github.com>
2024-03-08 20:04:29 +0800

    Add @CrossOrigin for front-end integration

--- demo/src/main/java/com/example/demo/controller/StudentController.java
+++ demo/src/main/java/com/example/demo/controller/StudentController.java
@@ -9,6 +9,7 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -23,6 +24,7 @@ import com.example.demo.service.StudentService;
 
 @RestController
 @RequestMapping("/api/students")
+@CrossOrigin("http://localhost:3000")
 public class StudentController {
     
     private StudentService studentService;
